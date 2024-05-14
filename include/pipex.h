e100107ee5a921db04717195bddc256e027c3c30
Raul Marcano Reimundez <rmarcano@c1r1s6.42madrid.com>
2024-05-14 19:35:07 +0200

    lib.h no leaks

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: rmarcano <rmarcano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 13:43:46 by rmarcano          #+#    #+#             */
-/*   Updated: 2024/05/13 11:33:45 by rmarcano         ###   ########.fr       */
+/*   Updated: 2024/05/14 19:23:03 by rmarcano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@
 void	ft_error(void);
 void	get_path(char ***all_path, char **env);
 void	get_real_path(char **all_path, char **cmd, char **real_path);
-void	free_array(char **array);
+void	free_array(char ***array);
 void	execute_cmd(char ***all_path, char *comand);
 
 	//PROCESSES
-void	parent_process(int *fd, char **argv, char **all_path);
-void	child_process(int *fd, char **argv, char **all_path);
+void	parent_process(int *fd, char **argv, char ***all_path);
+void	child_process(int *fd, char **argv, char ***all_path);
 
 #endif
